12d0e024cccd0c7f9c37f7be25cf72f9aaf0b589
Louis Qin <qyl0509@icloud.com>
2023-11-29 14:57:14 -0500

    Add some bounded support distributions.

--- score/distributions.py
+++ score/distributions.py
@@ -18,6 +18,9 @@ def symmetric_gaussian_mixture_2d():
 		D.Normal(torch.tensor([[-5., 5.], [0., 0.]]), torch.ones(2, 2)), 1)
 	return torch.distributions.mixture_same_family.MixtureSameFamily(mix, comp)
 
+def uniform_2d():
+	return torch.distributions.uniform.Uniform(torch.zeros(2), torch.ones(2))
+
 class GaussianMixtureStripe:
 	# TODO: make this a D.Distribution
 	def __init__(self):
@@ -28,3 +31,15 @@ class GaussianMixtureStripe:
 		uniform_samples = self.uniform_distribution.sample(sample_shape)
 		gaussian_mixture_samples = self.gaussian_mixture.sample(sample_shape)
 		return torch.stack((uniform_samples, gaussian_mixture_samples)).T
+	
+class BoxedStandardGaussian:
+	# TODO: make this a D.Distribution
+	def __init__(self):
+		self.standard_gaussian = standard_gaussian_2d()
+	
+	def sample(self, sample_shape):
+		assert len(sample_shape) == 1
+		samples = self.standard_gaussian.sample((sample_shape[0] * 3, ))
+		out_of_bounds = (samples > 1) | (samples < -1)
+		out_of_bounds = out_of_bounds[:, 0] | out_of_bounds[:, 1]
+		return samples[~out_of_bounds][:sample_shape[0]]
