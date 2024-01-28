4d1fa744d62179807b112d9041be2c9d1c18d5d7
Louis Qin <qyl0509@icloud.com>
2024-01-27 17:53:40 -0800

    Move distribution implementation to torch.distributions.Distribution.

--- score/distributions.py
+++ score/distributions.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.distributions as D
 
@@ -22,8 +24,7 @@ def symmetric_gaussian_mixture_2d():
 def uniform_2d():
 	return torch.distributions.uniform.Uniform(torch.zeros(2), torch.ones(2))
 
-class GaussianMixtureStripe:
-	# TODO: make this a D.Distribution
+class GaussianMixtureStripe(D.Distribution):
 	def __init__(self):
 		self.uniform_distribution = D.Uniform(0, 4)
 		self.gaussian_mixture = symmetric_gaussian_mixture_1d()
@@ -31,22 +32,21 @@ class GaussianMixtureStripe:
 	def sample(self, sample_shape):
 		uniform_samples = self.uniform_distribution.sample(sample_shape)
 		gaussian_mixture_samples = self.gaussian_mixture.sample(sample_shape)
-		return torch.stack((uniform_samples, gaussian_mixture_samples)).T
+		samples = torch.stack((uniform_samples, gaussian_mixture_samples))
+		return samples.permute(*range(1, samples.dim()), 0)
 	
-class BoxedStandardGaussian:
-	# TODO: make this a D.Distribution
+class BoxedStandardGaussian(D.Distribution):
 	def __init__(self):
 		self.standard_gaussian = standard_gaussian_2d()
 	
 	def sample(self, sample_shape):
-		assert len(sample_shape) == 1
-		samples = self.standard_gaussian.sample((sample_shape[0] * 3, ))
+		flattened_size = math.prod(sample_shape)
+		samples = self.standard_gaussian.sample((flattened_size * 3, ))
 		out_of_bounds = (samples > 1) | (samples < -1)
 		out_of_bounds = out_of_bounds[:, 0] | out_of_bounds[:, 1]
-		return samples[~out_of_bounds][:sample_shape[0]]
+		return samples[~out_of_bounds][:flattened_size].reshape((*sample_shape, 2))
 	
-class NoisyUniform2D:
-	# TODO: make this a D.Distribution
+class NoisyUniform2D(D.Distribution):
 	def __init__(self, noise_level):
 		self.uniform = uniform_2d()
 		self.standard_gaussian = standard_gaussian_2d()
